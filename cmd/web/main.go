8825e7a681540fc5b1208d9269a662cfa30fb14c
axelmaker <axelmaker@ya.ru>
2022-01-10 23:43:28 +0300

    add reservation summary

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/8thgencore/bookings/internal/config"
 	"github.com/8thgencore/bookings/internal/handlers"
+	"github.com/8thgencore/bookings/internal/models"
 	"github.com/8thgencore/bookings/internal/render"
 	"github.com/alexedwards/scs/v2"
 )
@@ -19,6 +21,8 @@ var session *scs.SessionManager
 
 // main is the main application function
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
 
 	// change this to true when in production
 	app.InProduction = false
